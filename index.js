48d07379ab6288d8262e54e49a67e2e73a54d884
KennethGHChan <KennethGHChan@gmail.com>
2023-09-06 23:08:57 -0400

    Adding to do list for core functions of the bot

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import { config } from 'dotenv';
 import { Client, GatewayIntentBits, Routes } from 'discord.js';
 import { REST } from '@discordjs/rest';
@@ -8,9 +7,16 @@ import { connectToDatabase } from './database.js';
 
 // npm run start:dev
 
+/** 
+Goals currently:
+1) When user uses command to upload a file, allow the user to add a description along with the file and the desired date and how much time before the designated date to have the bot DM this info
+2) Allow user to create a to do list, creating a command to add tasks, and using emotes to delete tasks and allow user to set priority of said tasks
+3) Add optional function that allows the bot to dm the user daily information such as reminders, weather or tasks.
+**/
 
 config();
 
+// Storing environment variables into normal variables
 const TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.guild_id;
@@ -23,6 +29,8 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ],
 });
+
+// Setting and using bot token to login while also connecting to the database
 
 const rest = new REST({ version: '10' }).setToken(TOKEN)
 
