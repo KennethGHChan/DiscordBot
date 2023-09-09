8fa528fe8600b8585b2f3b1c2cf093b3895b50a7
KennethGHChan <KennethGHChan@gmail.com>
2023-09-09 17:28:17 -0400

    Added schemas

---
+++ Schemas/user.js
@@ -0,0 +1,9 @@
+/**
+ * A database schema is a blueprint or structure that represents the logical configuration of all or part of a relational database. 
+ * It defines how data is organized and how relationships between data are handled. 
+ * In essence, a schema provides a visual representation of how different tables, views, stored procedures, and other database elements relate to each other.
+ */
+
+import mongoose from 'mongoose'
+
+//
